c3dd61158212b05f457f0e546ffef8c26e440efb
KimDongGi <kdkdongki1997@gmail.com>
2020-10-29 16:39:31 +0900

    [feat] Client access 허용 (cors 이슈 관련)

--- backend/src/main/java/com/kdk/KdkTemplateApplication.java
+++ backend/src/main/java/com/kdk/KdkTemplateApplication.java
@@ -1,12 +1,34 @@
 package com.kdk;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class KdkTemplateApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(KdkTemplateApplication.class, args);
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // FrontEnd PORT (React Client)
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
